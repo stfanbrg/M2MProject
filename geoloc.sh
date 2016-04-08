c456ac21ff90df6bd92cd52a2c2ac65d4f4bed92
jeanyves-b <guanach@gmail.com>
2016-04-08 20:51:38 +0000

    mise au point des entrées de geoloc.sh

--- geoloc.sh
+++ geoloc.sh
@@ -2,6 +2,7 @@
 #Xa Ya Za Xb Yb Zb Xc Yc Zc RSSIa RSSIb RSSIc id trame
 
 name=$(echo ${13}_$(date '+%d-%m-%Y').txt)
-echo ${14} $(date --utc '+%H:%M:%S') $(./calculator ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12}) #>> ${name}
+echo ${14} $(date --utc '+%H:%M:%S') $(./calculator ${1} ${2} ${4} ${5} ${7} ${8} ${10} ${11} ${12}) >> ${name}
+
 #sortie dans un fichier 
 #$id_$date.txt
